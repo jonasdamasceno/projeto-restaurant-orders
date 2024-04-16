296f73594d4b3bfe1629311fbeaed483fc535666
jonas <jonasmichael2021@outlook.com>
2024-04-16 15:32:51 -0300

    requesito 5

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -1,7 +1,8 @@
 from csv import DictReader
 from typing import Dict
+import csv
 
-from src.models.dish import Recipe
+from src.models.dish import Recipe, Dish
 from src.models.ingredient import Ingredient
 
 BASE_INVENTORY = "data/inventory_base_data.csv"
@@ -27,8 +28,37 @@ class InventoryMapping:
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe) -> bool:
-        pass
+        with open("data/inventory_base_data.csv", encoding="utf-8") as file:
+            inventory_data = csv.DictReader(file)
+            inventory = {}
+
+            for row in inventory_data:
+                ingredient_name = row["ingredient"]
+                initial_amount = int(row["initial_amount"])
+                inventory[ingredient_name] = initial_amount
+
+        return all(
+            ingredient.name in inventory
+            and inventory[ingredient.name] >= int(quantity)
+            for ingredient, quantity in recipe.items()
+        )
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if self.check_recipe_availability(recipe):
+            for ingredient in recipe:
+                self.inventory[ingredient] -= recipe[ingredient]
+
+        else:
+            raise ValueError
+
+if __name__ == "__main__":
+
+    teste = InventoryMapping()
+    test_dish2 = Dish("Carbonara", 20)
+    test_dish2.add_ingredient_dependency(Ingredient("ovo"), 10)
+    test_dish2.add_ingredient_dependency(Ingredient("bacon"), 10)
+    test_dish2.add_ingredient_dependency(Ingredient("limão"), 10)
+    test_dish2.add_ingredient_dependency(Ingredient("fermento"), 10)
+
+    teste.consume_recipe(test_dish2.recipe)
